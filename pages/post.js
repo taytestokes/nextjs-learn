23eeab5b775d2d00b2c6b933441d762afa447ac4
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-02 11:42:52 -0600

    sandboxed fetching data using the getInitialProps feature and the isomorphic-unfetch library to mimic the browsers fetch api

--- pages/post.js
+++ pages/post.js
@@ -1,17 +1,24 @@
-import { withRouter } from 'next/router';
 import Layout from '../components/Layout';
-
-const Content = withRouter(props => (
-    <div>
-        <h1>{props.router.query.title}</h1>
-        <p>This is content about the post</p>
-    </div>
-));
+import fetch from 'isomorphic-unfetch';
 
 const Post = props => (
     <Layout>
-        <Content />
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+        <img src={props.show.image.medium} />
     </Layout>
 );
+
+Post.getInitialProps = async function () {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+
+    console.log(`Fetched show: ${show.name}`);
+
+    return {
+        show
+    }
+};
 
 export default Post;
